156b3861c7b58a6491ff9831ce8fcf6250b8fe89
daniloliberatocardoso-afk <danilo.lcardoso@aluno.edu.es.gov.br>
2026-06-24 10:51:37 -0300

    Add basic PHP script to index.php

---
+++ aula 02 PHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> Aula 02 PHP </title>
+    </head>
+    <body>
+        <?php
+          $nome = "Danilo";
+          $idade = "16";
+          
+          echo "Meu nome é\n" .$nome . "\ne minha idade é\n" .$idade ."\nanos";
+        ?>
+
+    </body>
+</html>
